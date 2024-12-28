0d406b95abc894771b2e4f0b84b01b56fbd8ad6a
kakasoo <kscodebase@gmail.com>
2024-12-28 20:21:28 +0900

    feat: RemoveLastProperty and RemoveAfterDot

---
+++ src/types/RemoveAfterDot.ts
@@ -0,0 +1,25 @@
+import { ElementOf } from './ElementOf';
+
+export type RemoveAfterDot<T extends object, K extends string> = K extends `${infer First}.${infer Last}`
+  ? First extends keyof T
+    ? T[First] extends Array<any>
+      ? `${First}[*].${string}`
+      : T[First] extends object
+        ? `${First}.${RemoveAfterDot<T[First], Last>}`
+        : never
+    : First extends '[*]'
+      ? T extends Array<any>
+        ? RemoveAfterDot<ElementOf<T>, Last>
+        : never
+      : First extends `${infer Second extends string}[*]`
+        ? Second extends keyof T
+          ? T[Second] extends object
+            ? RemoveAfterDot<T[Second], Last>
+            : never
+          : never
+        : never
+  : K extends keyof T
+    ? T[K] extends Array<any>
+      ? `${K}[*].${string}`
+      : `${K}.${string}`
+    : never;
