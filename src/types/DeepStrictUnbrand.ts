6f3f4359775ddf7fa08c22e52ede9b62be91716d
kakasoo <kscodebase@gmail.com>
2025-01-26 15:21:12 +0900

    feat: add comments and divide private types in `DeepStrictUnbrand` type

--- src/types/DeepStrictUnbrand.ts
+++ src/types/DeepStrictUnbrand.ts
@@ -1,17 +1,19 @@
-type Primitive = string | number | boolean | symbol | null | undefined;
-type Unbrand<T extends Primitive & Record<any, any>> = T extends string & Record<any, any>
-  ? Extract<string, Omit<T, any>>
-  : T extends number & Record<any, any>
-    ? Extract<number, Omit<T, any>>
-    : T extends boolean & Record<any, any>
-      ? Extract<boolean, Omit<T, any>>
-      : T extends symbol & Record<any, any>
-        ? Extract<symbol, Omit<T, any>>
-        : T extends null & Record<any, any>
-          ? Extract<null, Omit<T, any>>
-          : T extends undefined & Record<any, any>
-            ? Extract<undefined, Omit<T, any>>
-            : T;
+namespace DeepStrictUnbrand {
+  export type Primitive = string | number | boolean | symbol | null | undefined;
+  export type Unbrand<T extends Primitive & Record<any, any>> = T extends string & Record<any, any>
+    ? string
+    : T extends number & Record<any, any>
+      ? number
+      : T extends boolean & Record<any, any>
+        ? boolean
+        : T extends symbol & Record<any, any>
+          ? symbol
+          : T extends null & Record<any, any>
+            ? null
+            : T extends undefined & Record<any, any>
+              ? undefined
+              : T;
+}
 
 /**
  * @title Type for Recursively Removing Branding Types.
@@ -42,10 +44,12 @@ export type DeepStrictUnbrand<T> =
     ? Array<Date>
     : T extends Array<infer I extends object>
       ? Array<DeepStrictUnbrand<I>>
-      : T extends Primitive & NonNullable<unknown>
-        ? Unbrand<T>
-        : T extends Date
-          ? T
+      : T extends DeepStrictUnbrand.Primitive & NonNullable<unknown> // If T is branding type.
+        ? DeepStrictUnbrand.Unbrand<T>
+        : T extends Date // Date is a type of object,
+          ? T // but it is not considered a branding type.
           : {
-              [K in keyof T]: T[K] extends object ? DeepStrictUnbrand<T[K]> : T[K];
+              [K in keyof T]: T[K] extends object
+                ? DeepStrictUnbrand<T[K]> // Recursively releases the branding type.
+                : T[K];
             };
