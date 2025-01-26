14261392ef16d4ae3a7894ec223a078bc5e15008
kakasoo <kscodebase@gmail.com>
2025-01-26 20:15:29 +0900

    feat: Add isSafe option to `DeepStrictObjectKeys` #4

--- src/types/DeepStrictObjectKeys.ts
+++ src/types/DeepStrictObjectKeys.ts
@@ -1,34 +1,44 @@
 import { DeepStrictUnbrand } from './DeepStrictUnbrand';
+import { Equal } from './Equal';
 import type { IsAny } from './IsAny';
 import type { IsUnion } from './IsUnion';
 import type { ValueType } from './ValueType';
 
+type a = DeepStrictObjectKeys.Infer<{ a: Date }>;
+
 namespace DeepStrictObjectKeys {
   export type Infer<
-    T extends object,
-    Joiner extends {
-      array: string;
-      object: string;
-    } = {
-      array: '[*]';
-      object: '.';
-    },
-    P extends keyof T = keyof T,
+    Target extends object,
+    Joiner extends { array: string; object: string } = { array: '[*]'; object: '.' },
+    IsSafe extends boolean = true,
+    P extends keyof Target = keyof Target,
   > = P extends string
-    ? IsUnion<T[P]> extends true
-      ? P
-      : T[P] extends Array<infer Element extends object>
-        ? P | `${P}${Joiner['array']}${Joiner['object']}${Infer<Element, Joiner>}`
-        : T[P] extends ValueType
+    ? IsUnion<Target[P]> extends true
+      ? Equal<IsSafe, true> extends true
+        ? P
+        : // If a user wants to explore a type that is a union of primitive types and object types.
+          | P
+            | (Target[P] extends infer E
+                ? E extends ValueType
+                  ? P
+                  : E extends object
+                    ? E extends Array<infer _Element extends object>
+                      ? `${P}${Joiner['array']}${Joiner['object']}${Infer<_Element, Joiner, IsSafe>}` // recursive
+                      : `${P}${Joiner['object']}${Infer<E, Joiner, IsSafe>}` // recursive
+                    : never // Remove all primitive types of union types.
+                : never)
+      : Target[P] extends Array<infer Element extends object>
+        ? P | `${P}${Joiner['array']}${Joiner['object']}${Infer<Element, Joiner, false>}`
+        : Target[P] extends ValueType
           ? P
-          : IsAny<T[P]> extends true
+          : IsAny<Target[P]> extends true
             ? P
-            : T[P] extends object
-              ? T[P] extends Array<infer _Element>
+            : Target[P] extends object
+              ? Target[P] extends Array<infer _Element>
                 ? P
-                : T[P] extends Record<string, never>
+                : Target[P] extends Record<string, never>
                   ? `${P}`
-                  : `${P}` | `${P}${Joiner['object']}${Infer<T[P], Joiner>}`
+                  : `${P}` | `${P}${Joiner['object']}${Infer<Target[P], Joiner, false>}`
               : never
     : never;
 }
@@ -44,16 +54,20 @@ namespace DeepStrictObjectKeys {
  * type Example1 = DeepStrictObjectKeys<{ a: { b: 1; c: 2 } }>; // "a" | "a.b" | "a.c"
  * type Example2 = DeepStrictObjectKeys<{ a: { b: 1; c: { d: number }[] } }>; // "a" | "a.b" | "a.c" | "a.c[*].d"
  * ```
+ * @template Target Destination type for which you want to pull a key
+ * @template Joiner It means what symbol to connect when recursively spinning superimposed types.
+ * @template IsSafe When a key is a combination type of a primitive type and an object, it means whether to perform a recursive search or not.
  */
 export type DeepStrictObjectKeys<
-  T extends object,
+  Target extends object,
   Joiner extends { array: string; object: string } = {
     array: '[*]';
     object: '.';
   },
+  IsSafe extends boolean = true,
 > =
-  DeepStrictUnbrand<T> extends Array<infer Element>
+  DeepStrictUnbrand<Target> extends Array<infer Element>
     ? Element extends object
-      ? `${Joiner['array']}.${DeepStrictObjectKeys<Element, Joiner>}`
+      ? `${Joiner['array']}.${DeepStrictObjectKeys<Element, Joiner, IsSafe>}`
       : `${Joiner['array']}.${keyof Element extends string ? keyof Element : never}`
-    : DeepStrictObjectKeys.Infer<DeepStrictUnbrand<T>, Joiner, Extract<keyof T, keyof DeepStrictUnbrand<T>>>;
+    : DeepStrictObjectKeys.Infer<DeepStrictUnbrand<Target>, Joiner, IsSafe>;
--- test/types/DeepStrictObjectKeys.ts
+++ test/types/DeepStrictObjectKeys.ts
@@ -1,168 +1,580 @@
 import { ok } from 'node:assert';
-import test from 'node:test';
+import test, { describe } from 'node:test';
 import typia, { tags } from 'typia';
 import { DeepStrictObjectKeys, Equal } from '../../src';
 
-test('Date props', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: Date;
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
+describe('Date props', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: Date;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: Date;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
 });
 
-test('union with Date and string', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: Date | string;
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
+describe('union with Date and string', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: Date | string;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: Date | string;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
 });
 
-test('branding type of string (typia)', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: string & tags.Format<'date-time'>;
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
+describe('branding type of string (typia)', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: string & tags.Format<'date-time'>;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: string & tags.Format<'date-time'>;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
 });
 
-test('branding type of string', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: number & { unit: 'dollar' | 'won' };
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('primitive type and object type / apply conservative type reasoning', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: number | { value: number; unit: 'dollar' | 'won' };
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('primitive type and array type / apply conservative type reasoning', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: number | Array<{ value: number; unit: 'dollar' | 'won' }>;
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('union type with object and array / apply conservative type reasoning', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: { value: number; unit: 'dollar' | 'won' } | Array<{ value: number; unit: 'dollar' | 'won' }>;
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('nested object with 2 depth', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: { value: number; unit: 'dollar' | 'won' };
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other' | 'prop.value' | 'prop.unit'>;
-  ok(typia.random<Answer>());
-});
-
-test('nested object with 3 depth', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: {
-      value: number;
-      unit: 'dollar';
-      country: {
-        name: string;
-        location: string;
-      };
-    };
-    other: number;
-  }>;
-  type Answer = Equal<
-    Question,
-    | 'prop' // depth 1
-    | 'other'
-    | 'prop.value' // depth 2
-    | 'prop.unit'
-    | 'prop.country'
-    | 'prop.country.name' // depth 3
-    | 'prop.country.location'
-  >;
-  ok(typia.random<Answer>());
-});
-
-test('array property', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: number[];
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('union array property', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: (number | string)[];
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('array of branding type of string property', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: (string & { unit: 'dollar' | 'won' })[];
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('array of branding type of string property (typia)', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: (string & tags.Format<'uuid'>)[];
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other'>;
-  ok(typia.random<Answer>());
-});
-
-test('array of branding type of string property', () => {
-  type Question = DeepStrictObjectKeys<{
-    prop: { value: number; unit: 'dollar' | 'won' }[];
-    other: number;
-  }>;
-  type Answer = Equal<Question, 'prop' | 'other' | 'prop[*].unit' | 'prop[*].value'>;
-  ok(typia.random<Answer>());
-});
-
-test('key in an array', () => {
+describe('branding type of string', () => {
+  test('normal: isNotSafe', () => {});
   type Question = DeepStrictObjectKeys<
     {
-      prop: { value: number; unit: 'dollar' | 'won' }[];
+      prop: number & { unit: 'dollar' | 'won' };
       other: number;
-    }[]
+    },
+    {
+      array: '[*]';
+      object: '.';
+    },
+    false
   >;
-  type Answer = Equal<Question, '[*].prop' | '[*].other' | '[*].prop[*].unit' | '[*].prop[*].value'>;
+  type Answer = Equal<Question, 'prop' | 'other'>;
   ok(typia.random<Answer>());
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number & { unit: 'dollar' | 'won' };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
 });
 
-test('a key in a two-dimensional array', () => {
-  type Question = DeepStrictObjectKeys<
-    {
-      prop: { value: number; unit: 'dollar' | 'won' }[];
-      other: number;
-    }[][]
-  >;
-  type Answer = Equal<Question, '[*].[*].prop' | '[*].[*].other' | '[*].[*].prop[*].unit' | '[*].[*].prop[*].value'>;
-  ok(typia.random<Answer>());
+describe('primitive type and object type / apply conservative type reasoning', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number | { value: number; unit: 'dollar' | 'won' };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop.value' | 'prop.unit'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number | { value: number; unit: 'dollar' | 'won' };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('primitive type and array type / apply conservative type reasoning', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number | Array<{ value: number; unit: 'dollar' | 'won' }>;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop[*].value' | 'prop[*].unit'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number | Array<{ value: number; unit: 'dollar' | 'won' }>;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('union type with object and array / apply conservative type reasoning', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' } | Array<{ value: number; unit: 'dollar' | 'won' }>;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop.value' | 'prop.unit' | 'prop[*].value' | 'prop[*].unit'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' } | Array<{ value: number; unit: 'dollar' | 'won' }>;
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('nested object with 2 depth', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop.value' | 'prop.unit'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop.value' | 'prop.unit'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('nested object with 3 depth', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: {
+          value: number;
+          unit: 'dollar';
+          country: {
+            name: string;
+            location: string;
+          };
+        };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<
+      Question,
+      | 'prop' // depth 1
+      | 'other'
+      | 'prop.value' // depth 2
+      | 'prop.unit'
+      | 'prop.country'
+      | 'prop.country.name' // depth 3
+      | 'prop.country.location'
+    >;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: {
+          value: number;
+          unit: 'dollar';
+          country: {
+            name: string;
+            location: string;
+          };
+        };
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<
+      Question,
+      | 'prop' // depth 1
+      | 'other'
+      | 'prop.value' // depth 2
+      | 'prop.unit'
+      | 'prop.country'
+      | 'prop.country.name' // depth 3
+      | 'prop.country.location'
+    >;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('array property', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: number[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('union array property', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: (number | string)[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: (number | string)[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('array of branding type of string property', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: (string & { unit: 'dollar' | 'won' })[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: (string & { unit: 'dollar' | 'won' })[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('array of branding type of string property (typia)', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: (string & tags.Format<'uuid'>)[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: (string & tags.Format<'uuid'>)[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('array of branding type of string property', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' }[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop[*].unit' | 'prop[*].value'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' }[];
+        other: number;
+      },
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, 'prop' | 'other' | 'prop[*].unit' | 'prop[*].value'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('key in an array', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' }[];
+        other: number;
+      }[],
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, '[*].prop' | '[*].other' | '[*].prop[*].unit' | '[*].prop[*].value'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' }[];
+        other: number;
+      }[],
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, '[*].prop' | '[*].other' | '[*].prop[*].unit' | '[*].prop[*].value'>;
+    ok(typia.random<Answer>());
+  });
+});
+
+describe('a key in a two-dimensional array', () => {
+  test('normal: isNotSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' }[];
+        other: number;
+      }[][],
+      {
+        array: '[*]';
+        object: '.';
+      },
+      false
+    >;
+    type Answer = Equal<Question, '[*].[*].prop' | '[*].[*].other' | '[*].[*].prop[*].unit' | '[*].[*].prop[*].value'>;
+    ok(typia.random<Answer>());
+  });
+
+  test('normal: isSafe', () => {
+    type Question = DeepStrictObjectKeys<
+      {
+        prop: { value: number; unit: 'dollar' | 'won' }[];
+        other: number;
+      }[][],
+      {
+        array: '[*]';
+        object: '.';
+      },
+      true
+    >;
+    type Answer = Equal<Question, '[*].[*].prop' | '[*].[*].other' | '[*].[*].prop[*].unit' | '[*].[*].prop[*].value'>;
+    ok(typia.random<Answer>());
+  });
 });
