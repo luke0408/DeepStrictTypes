ac18aa8a7c2dc2fdebb07d9de8bdc02ea78c9104
zjvl324 <zjvl324@gmail.com>
2025-01-29 11:09:38 +0000

    feat: include and exclude filtering option

--- test/helpers/TestTreeLoader.ts
+++ test/helpers/TestTreeLoader.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 export namespace TestTreeLoader {
   export type TestTree = { [dirname: string]: TestTree | string[] };
+  export type FilterOption = { exclude?: string[]; include?: string[] };
 
   const FILES_KEY = 'files';
 
@@ -10,7 +11,12 @@ export namespace TestTreeLoader {
     return entry.name.startsWith('.');
   };
 
-  const isTestFile = (entry: fs.Dirent): boolean => {
+  const isTestFile = (dir: string, entry: fs.Dirent, options: FilterOption): boolean => {
+    const fullPath = path.join(dir, entry.name)
+    const isIncluded = options.include && options.include.some((incPath) => fullPath.includes(incPath)) && entry.name.endsWith('.js');
+    if (isIncluded) return true;
+    if (!isIncluded && options.include) return false;
+    if (options.exclude && options.exclude.some((exPath) => fullPath.includes(exPath))) return false;
     return entry.isFile() && entry.name.endsWith('.js') && entry.name !== 'index.js';
   };
 
@@ -25,7 +31,7 @@ export namespace TestTreeLoader {
     (currentTree[FILES_KEY] as string[]).push(entry.name);
   };
 
-  const traverse = (dir: string, currentTree: TestTree) => {
+  const traverse = (dir: string, currentTree: TestTree, options: FilterOption) => {
     const entries = fs.readdirSync(dir, { withFileTypes: true });
     let hasContent = false;
 
@@ -38,12 +44,12 @@ export namespace TestTreeLoader {
 
       if (isTestDirectory(entry)) {
         const subtree: TestTree = {};
-        traverse(fullPath, subtree);
+        traverse(fullPath, subtree, options);
         if (Object.keys(subtree).length > 0) {
           currentTree[entry.name] = subtree;
           hasContent = true;
         }
-      } else if (isTestFile(entry)) {
+      } else if (isTestFile(dir, entry, options)) {
         addFileToTree(currentTree, entry);
         hasContent = true;
       }
@@ -54,10 +60,10 @@ export namespace TestTreeLoader {
     }
   };
 
-  export const load = (): TestTree => {
+  export const load = (dir: string, options: FilterOption): TestTree => {
     const tree: TestTree = {};
-    const currentDir = __dirname;
-    traverse(currentDir, tree);
+    const currentDir = dir;
+    traverse(currentDir, tree, options);
     return tree;
   };
 }
--- test/index.ts
+++ test/index.ts
@@ -11,7 +11,7 @@ interface IOptions {
 const getOptions = () =>
   ArgumentParser.parse<IOptions>(async (command, prompt, action) => {
     command.option('--include <string...>', 'include feature files');
-    command.option('--exculde <string...>', 'exclude feature files');
+    command.option('--exclude <string...>', 'exclude feature files');
 
     prompt;
 
@@ -22,31 +22,7 @@ const getOptions = () =>
 
 async function main(): Promise<void> {
   const options: IOptions = await getOptions();
-
-  console.log('Include: ', options.include);
-  console.log('Exclude: ', options.exclude);
-
-  /**
-   * 0. include와 exclude 둘다 없는 경우
-   *   => 전체 테스트 실행
-   *
-   * 1. include만 있는 경우
-   *  1-1. include가 file명인 경우
-   *    => 단일 테스트 파일 실행
-   *  1-2. include가 package명인 경우
-   *    => package 내부 test 전체 실행
-   *
-   * 2. exclude만 있는 경우
-   *  2-1. exclude가 file명인 경우
-   *    => 전체에서 해당 테스트 파일 제외 후 실행
-   *  2-2. exclude가 package명인 경우
-   *    => 전체에서 해당 패키지 제외 후 실행
-   *
-   * 3. 둘다 있는 경우
-   *  3-1. include에 package가 있으며 exclude에 file이 있는 경우
-   *    => include한 패키지에서 exclude file 제외 후 실행
-   */
-  const testSet = TestTreeLoader.load();
+  const testSet = TestTreeLoader.load(__dirname, options);
   console.log(testSet);
 
   async function allTest() {
