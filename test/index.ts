cd7d5b919734f6deaeb50008f23318e2eb77b872
zjvl324 <zjvl324@gmail.com>
2025-01-30 07:30:07 +0000

    feat: test run code

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,3 @@
-import fs from 'fs';
-import path from 'path';
 import { ArgumentParser } from './helpers/ArgumentParser';
 import { TestFileLoader } from './helpers/TestFileLoader';
 
@@ -23,30 +21,9 @@ const getOptions = () =>
 async function main(): Promise<void> {
   const options: IOptions = await getOptions();
   const testSet = TestFileLoader.load(__dirname, options);
-  console.log(testSet);
 
-  async function allTest() {
-    /**
-     * test code for types
-     */
-    const types = fs.readdirSync(path.join(__dirname, './types'));
-    for await (const filename of types) {
-      if (filename.endsWith('.js')) {
-        const filePath = path.join(__dirname, 'types', filename);
-        await import(filePath);
-      }
-    }
-
-    /**
-     * test code for functions
-     */
-    const functions = fs.readdirSync(path.join(__dirname, './functions'));
-    for await (const filename of functions) {
-      if (filename.endsWith('.js')) {
-        const filePath = path.join(__dirname, 'functions', filename);
-        await import(filePath);
-      }
-    }
+  for await (const test of testSet) {
+    await import(test);
   }
 }
 
